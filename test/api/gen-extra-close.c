88d8e0ab62b381e86f2ed51bf4b7eab52e43ee21
Greg A. Woods <woods@robohack.ca>
2024-03-30 20:33:20 -0700

    test/api/: fix this test

--- test/api/gen-extra-close.c
+++ test/api/gen-extra-close.c
@@ -2,18 +2,31 @@
  * we get the expected error */
 
 #include <yajl/yajl_gen.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
-#define CHK(x) if (x != yajl_gen_status_ok) return 1;
+#define CHK(x) if (x != yajl_gen_status_ok) {                           \
+        fprintf(stderr, "intermediate step failed!");                   \
+        exit(2);                                                        \
+    }
 
-int main(void) {
-  yajl_gen yg;
-  yajl_gen_status s;
 
-  yg = yajl_gen_alloc(NULL);
-  CHK(yajl_gen_map_open(yg));
-  CHK(yajl_gen_map_close(yg));
-  s = yajl_gen_map_close(yg);
+int
+main(void)
+{
+    yajl_gen yg;
+    yajl_gen_status s;
 
-  return (yajl_gen_generation_complete == s);
+    yg = yajl_gen_alloc(NULL);
+    CHK(yajl_gen_map_open(yg));
+    CHK(yajl_gen_map_close(yg));
+    s = yajl_gen_map_close(yg);
+
+    if (yajl_gen_generation_complete == s) {
+        exit(0);
+    }
+    fprintf(stderr, "s = %d (should be: %d)\n", s, yajl_gen_generation_complete);
+
+    exit(1);
 }
